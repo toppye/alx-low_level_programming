e90369a0538785ee12e4a51ac82f14e093b8f621
toppye <muiznero@gmail.com>
2022-12-09 15:19:48 -0800

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,10 +8,10 @@ int main(void)
 {
 	char ch;
 
-	for (ch = 'a'; ch = 'z'; ch++)
+	for (ch = 'a'; (ch = 'z'); ch++)
 		putchar(ch);
 
-	for (ch = 'A'; ch <= 'Z'; ch++)
+	for (ch = 'A'; (ch <= 'Z'); ch++)
 		putchar(ch);
 
 	putchar('\n');

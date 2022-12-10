87e570cf26087b72f8905dc0350c66bab8a871d8
toppye <muiznero@gmail.com>
2022-12-10 05:29:21 -0800

    The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -32,14 +32,14 @@ int main(void)
 			{
 				if (c != d && d != e && e < d && d < c)
 				{
+					putchar('0' + e);
 					putchar('0' + d);
-					putchar('0' + c);
 					putchar('0' + c);
 
 					if (c + d + e != 9 + 8 + 7)
 					{
-					putchar(',');
-					putchar(' ');
+						putchar(',');
+						putchar(' ');
 					}
 				}
 

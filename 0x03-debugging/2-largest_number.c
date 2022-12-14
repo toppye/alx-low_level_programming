2eb1ebdeeb9495ee67909457c4de4508659b97b0
toppye <muiznero@gmail.com>
2022-12-14 13:35:11 -0800

    irdxh

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,13 +14,13 @@ int largest_number(int a, int b, int c)
 
 	if (a > b && b > c)
 	{
-		largest = a
+		largest = a;
 	}
 	else if (b > a && a > c)
 	{
 		largest = b;
 	}
-	else
+	else if (c > b && b > a)
 	{
 		largest = c;
 	}

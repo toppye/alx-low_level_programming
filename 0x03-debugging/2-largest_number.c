5b8ee4a0e3cee1f797ab9bc97e694eafeeeabe6a
toppye <muiznero@gmail.com>
2022-12-14 15:19:19 -0800

    greater check

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,13 +16,17 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (b > a && a > c)
+	else if (b > c && b > a)
 	{
 		largest = b;
 	}
-	else if (c > b && b > a)
+	else if (c > b)
 	{
 		largest = c;
+	}
+	else
+	{
+		largest = b;
 	}
 
 	return (largest);

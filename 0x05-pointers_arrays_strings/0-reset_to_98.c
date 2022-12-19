093478d695bad039295e1a049c1969e55d8712b0
toppye <muiznero@gmail.com>
2022-12-19 07:04:38 -0800

    0. 98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * reset_to_98
- * Return: 0 is successs
+ * reset_to_98 - takes the pointer and updates the value
+ * @n: int to check
+ * Return: 0 is success
  */
-
 
 void reset_to_98(int *n)
 {
